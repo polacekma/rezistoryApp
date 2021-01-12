94a019f4601ffc9e71608270f2b8e9ff64a31a7f
polacekma <59505547+polacekma@users.noreply.github.com>
2021-01-12 10:01:16 +0100

    oprava

--- app/logika.js
+++ app/logika.js
@@ -6,10 +6,10 @@ function nearZero(a) {
   return Math.abs(a) < 1e-9;
 }
 
-// Represents a linear combination of a number of variables and a constant.
-// coeffs: a list of coefficients for the variables in vars.
-// vars: a list of strings naming variables.
-// c: constant term
+// Představuje lineární kombinaci několika proměnných a konstanty.
+// coeffs: seznam koeficientů pro proměnné ve vars.
+// vars: seznam řetězců pojmenujících proměnné.
+// c: konstantní člen
 function AffineExpression(coeffs, vars, c) {
   this.coeffs = [];
   this.empty = true;
@@ -22,7 +22,7 @@ function AffineExpression(coeffs, vars, c) {
   this.constant = c;
 }
 
-// Return the coefficient associated with the variable v.
+// vrácení coef s prefixem v.
 function AE_getCoefficient(v) {
   if (v in this.coeffs) {
     return this.coeffs[v];
@@ -31,8 +31,7 @@ function AE_getCoefficient(v) {
   }
 }
 
-// Return a variable we can pivot on. If this AffineExpression has no
-// variables, then return null.
+
 function AE_getLargestPivot() {
   // For numerical stability, choose the largest pivot.
   var maxPivot = 0;
@@ -50,14 +49,12 @@ function AE_getScalarValue() {
   return this.constant;
 }
 
-// Return true iff this expression consists of only a constant term (no
-// variables).
+
 function AE_isScalar() {
   return this.empty;
 }
 
-// Return a new AffineExpression containing the value of v in terms of the
-// other variables. Assumes that v has a nonzero coefficient.
+
 function AE_solveFor(v) {
   var newCoeffs = [];
   var newVars = [];
@@ -71,14 +68,12 @@ function AE_solveFor(v) {
   return new AffineExpression(newCoeffs, newVars, -this.constant / vCoeff);
 }
 
-// Substitute, in the current expression, the variable V with the expression
-// EXPR and return the resulting expression.
+
 function AE_substitute(v, expr) {
   var newCoeffs = [];
   var newConstant = 0;
   var oldVCoeff = 0;
-  // Copy the terms over from the existing expression, except for the one
-  // we're substituting away.
+
   for (var w in this.coeffs) {
     if (w == v) {
       oldVCoeff = this.coeffs[w];
@@ -87,7 +82,7 @@ function AE_substitute(v, expr) {
     }
   }
   newConstant = this.constant;
-  // Add the expression to substitute, scaled appropriately.
+
   for (var x in expr.coeffs) {
     if (x in newCoeffs) {
       newCoeffs[x] += expr.coeffs[x] * oldVCoeff;
@@ -124,59 +119,56 @@ AffineExpression.prototype.solveFor = AE_solveFor;
 AffineExpression.prototype.substitute = AE_substitute;
 AffineExpression.prototype.toString = AE_toString;
 
-// Solves a system of simultaneous linear equations. The equations are given by
-// X == 0 for each X in SYSTEM (a list of AffineExpressions).
+// Řeší systém simultánních lineárních rovnic. Rovnice jsou dány vztahem
+// X == 0 pro každé X v SYSTÉMU (seznam AffineExpressions).
 //
-// Throw inconsistentSystemError if the system is inconsistent.
+// Vyhodit inconsistentSystemError, pokud je systém nekonzistentní.
 //
-// Otherwise, return an array with a key for each variable. If the variable has
-// a unique solution S, then the associated value is S. Otherwise, if the
-// variable is underconstrained, then the associated value is NaN.
+// V opačném případě vraťte pole s klíčem pro každou proměnnou. Pokud proměnná má
+// jedinečné řešení S, pak přidružená hodnota je S. Jinak, pokud
+// proměnná je nedostatečně omezená, přidružená hodnota je pak NaN.
 function solveLinearSystem(system) {
   var backVars = [];
   var backExprs = [];
 
-  // Perform forward elimination.
+  // zpětná kontrola
   for (var i = 0; i < system.length; i++) {
     var e = system[i];
     var pivotVar = e.getLargestPivot();
     if (pivotVar !== null && !nearZero(e.getCoefficient(pivotVar))) {
-      // Eliminate the pivot variable
+      //
       var subExpr = e.solveFor(pivotVar);
-      // Remember the variable and an expression for it in terms of other
-      // variables, for later substitution.
+      //
       backVars.push(pivotVar);
       backExprs.push(subExpr);
-      // Substitute in all subsequent equations.
+      //
       for (var j = i + 1; j < system.length; j++) {
         system[j] = system[j].substitute(pivotVar, subExpr);
       }
     } else {
-      // There's no pivot here. We have an equation of the form c == 0,
-      // for some constant c. That equation is either a tautology or a
-      // contradiction.
+      //  žádný pivot.  rovnice ve tvaru c == 0,
+      // pro nějakou konstantu c. Tou rovnicí je buď tautologie, nebo error
       if (nearZero(e.constant)) {
-        // This equation is redundant, we'll just ignore it
+
       } else {
-        // This equation yields an inconsistent system!
+
         throw inconsistentSystemError;
       }
     }
   }
-  // Perform back substitution.
+
   for (var m = backVars.length - 1; m >= 0; m--) {
     for (var n = m + 1; n < backVars.length; n++) {
       backExprs[m] = backExprs[m].substitute(backVars[n], backExprs[n]);
     }
   }
-  // Copy the output into a new array and return it.
+  // Zkopírujte výstup do nového pole a vrati jej.
   var soln = [];
   for (var v in backVars) {
     if (backExprs[v].isScalar()) {
       soln[backVars[v]] = backExprs[v].getScalarValue();
     } else {
-      // We don't have an explicit expression that doesn't depend on
-      // other variables. This variable is underconstrained.
+
       soln[backVars[v]] = NaN;
     }
   }
--- app/util.js
+++ app/util.js
@@ -3,8 +3,7 @@ var permanentMessage = '';
 var transientMessage = '';
 var lastMessageShown = '';
 
-// Print the specified status message. If isTransient is not set, the message
-// is saved and reappears after all any other transient messages are cleared.
+
 function printMessage(msg, isTransient) {
   if (isTransient) {
     transientMessage = msg;
@@ -22,25 +21,24 @@ function printOutput(msg) {
   document.getElementById("leadoutput").innerHTML = msg;
 }
 
-// Return dx^2 + dy^2.
+
 function normSquared(dx, dy) {
   return dx * dx + dy * dy;
 }
 
-// Return the extreme value (max or min) attained by a x^2 + b x + c
+
 function quadraticMinimum(a, b, c) {
-  // Compute location of minimum
+
   var x = - b / (2 * a);
   return a * x * x + b * x + c;
 }
 
-// Return the lesser of a and b.
+
 function min(a, b) {
   return (a < b) ? a : b;
 }
 
-// Return the square of the minimum length from the point (x, y) to the segment
-// (x1, y1) to (x2, y2).
+
 function minimumDistanceSquaredToSegment(x, y, x1, y1, x2, y2) {
   var cx = x2 - x1;
   var cy = y2 - y1;
